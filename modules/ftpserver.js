abf3cd181c50025022a514ac6c13d52efa5fb6da
Blasstah <lukpaw66@gmail.com>
2024-07-13 01:30:31 +0200

    FTP Server Full (in working state)

--- modules/ftpserver.js
+++ modules/ftpserver.js
@@ -13,8 +13,7 @@ class FTPServerModule {
             pasv_start: 3002,
             pasv_end: 3050,
             username: "admin",
-            password: "123321#",
-            maxConnections: 1,
+            password: "123321#"
         }
 
         this.connections = []
@@ -28,6 +27,8 @@ class FTPServerModule {
         }
 
         this.initializeServer = () => {
+            if(!this.settings.enabled) return;
+
             if(this.server) this.server.close();
 
             this.server = null;
@@ -60,6 +61,7 @@ class FTPServerModule {
                     }
                 }, 1000)
                 */
+
                 conn.on('command:user', (user, success, failure) => {
                     if(user != this.settings.username) {
                         failure();
@@ -82,10 +84,64 @@ class FTPServerModule {
                 this.refreshConnections()
             })
 
+            this.server.debugging = 4;
             if(this.settings.enabled)
                 this.server.listen(this.settings.port);
         }
         this.initializeServer();
+
+        this.registerSecureSocket = (socket) => {
+            socket.on("ftp_settings_update", (payload) => {
+                this.settings.enabled = payload.enabled ? true : false;
+                this.settings.host = payload.host;
+
+                if(!isNaN(payload.port))
+                    this.settings.port = payload.port;
+                else socket.emit("force_reload", {message: "Couldn't update FTP settings!", type: "error"})
+
+                if(!isNaN(payload.pasv_start))
+                    this.settings.pasv_start = payload.pasv_start;
+                else socket.emit("force_reload", {message: "Couldn't update FTP settings!", type: "error"})
+
+                if(!isNaN(payload.pasv_end))
+                    this.settings.pasv_end = payload.pasv_end;
+                else socket.emit("force_reload", {message: "Couldn't update FTP settings!", type: "error"})
+
+                context.saveConfig("ftpserver", this.settings);
+                this.context.showNotif(socket, "Updated FTP settings!", "success")
+            });
+
+            socket.on("ftp_credentials", (payload) => {
+                let username = payload.username;
+                let password = payload.password;
+
+                if(typeof(username) != "string") {
+                    socket.emit("force_reload", {message: "Bad username!", type: "error"})
+                    return;
+                }
+
+                if(typeof(password) != "string") {
+                    socket.emit("force_reload", {message: "Bad password!", type: "error"})
+                    return;
+                }
+
+                if(username.length < 5) {
+                    socket.emit("force_reload", {message: "Username requires at least 5 characters!", type: "error"})
+                    return;
+                }
+
+                if(password.length < 5) {
+                    socket.emit("force_reload", {message: "Password requires at least 5 characters!", type: "error"})
+                    return;
+                }
+
+                this.settings.username = username;
+                this.settings.password = password;
+                context.saveConfig("ftpserver", this.settings);
+
+                socket.emit("force_reload", {message: "Successfully updated FTP credentials!", type: "success"})
+            })
+        }
 
         this.context.addEntryPoint("/ftpserver", (req, res, data) => {
             data.ftp_server = { ...this.settings };
@@ -95,6 +151,7 @@ class FTPServerModule {
             data.connections = []
             for(let el of this.connections) {
                 if(!el.socket) continue;
+                console.log(el)
                 data.connections.push(el.socket.remoteAddress);
             }
 
